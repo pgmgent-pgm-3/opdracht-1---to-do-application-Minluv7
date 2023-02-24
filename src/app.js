5565e0a6c83821cb56ea9e5e6534689bf46132f5
Nese Yildirim <neseyild@student.arteveldehs.be>
2023-02-24 11:10:56 +0100

    week 2

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import express from "express";
 import path from "path";
 import * as dotenv from "dotenv";
 dotenv.config();
+import { create } from "express-handlebars";
+import { SOURCE_PATH } from "./constants.js";
 
 //create express app
 const app = express();
@@ -10,12 +12,23 @@ const app = express();
 //serve static file
 app.use(express.static("public"));
 
+//------------ HANDLEBARS -----------//
+
+// create an instance of express-handlebars
+const hbs = create({
+  extname: "hbs",
+});
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", path.join(SOURCE_PATH, "views"));
+
 //define port, use 3000 if no env variable is set
 const port = process.env.PORT || 3000;
 
+// ----------- ROUTES -------------- //
 app.get("/", (req, res) => {
-  const indexHTML = path.resolve("src", "views", "index.html");
-  res.sendFile(indexHTML);
+  res.render("home");
 });
 
 //start the app
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+// file contains some constants that will be used elsewhere
+import path from "path";
+export const SOURCE_PATH = path.resolve("src");
